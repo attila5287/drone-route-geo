1cea40481a2f004644838aec592dfc90cf85e2be
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-26 01:16:13 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Map from './Map'
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw";
+
+function App() {
+
+  return (
+    <>
+      <Map token={MAPBOX_TOKEN}/>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
+  mapboxAccessToken:
+    "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw",
+});
